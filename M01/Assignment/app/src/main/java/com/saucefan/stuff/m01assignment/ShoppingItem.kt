ac4c615751e541ca1c615888be710d7df055ba79
jack seymour <stilljack@gmail.com>
2019-09-09 12:57:24 -0700

    i think this is what's being requested of us before the recycler view -- i think

--- M01/Assignment/app/src/main/java/com/saucefan/stuff/m01assignment/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/saucefan/stuff/m01assignment/ShoppingItem.kt
@@ -1,22 +1,34 @@
 package com.saucefan.stuff.m01assignment
 
 import android.util.Log
+import android.database.sqlite.SQLiteOpenHelper
 
-open class ShoppingItem(val colorId:Int, val productName:String) {
+
+
+open class ShoppingItem(open val colorId:Int, val productName:String) {
     var getPN = productName
-    fun getDisplayName(): String {
+    open fun getDisplayName(): String {
         return getPN
     }
 }
 
 class Electronics(colorId:Int, productName:String): ShoppingItem(colorId, productName) {
-
+    override val colorId = 104444
+    override fun getDisplayName(): String {
+        return "$getPN+electronics"
+    }
 }
 
 class Groceries(colorId:Int, productName:String): ShoppingItem(colorId, productName) {
-
+    override val colorId = 555500
+    override fun getDisplayName(): String {
+        return "$getPN+Groceries"
+    }
 }
 
 class Dresses(colorId:Int, productName:String): ShoppingItem(colorId, productName) {
-
+    override val colorId = 660066
+    override fun getDisplayName(): String {
+        return "$getPN+electronics"
+    }
 }
