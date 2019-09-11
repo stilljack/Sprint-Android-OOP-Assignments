58478aa79ebf57f432a6b75da9882331b8cd10a7
jack seymour <stilljack@gmail.com>
2019-09-11 16:41:17 -0700

    pretty neat! really want to do more with rxJava and reactive programming

--- M03/Assignment/app/src/main/java/com/saucefan/stuff/mo3generics/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/mo3generics/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.saucefan.stuff.mo3generics
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ActionMode
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.saucefan.stuff.mo3generics.mockApi.Companion.getMovieList
 import io.reactivex.Observable
-import java.lang.reflect.Type
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        getMovieList().subscribe() {
+            println(it)
+        }
         /*    Task 1
       Create a simple example of a mutableList that takes strings as parameters.
       Try adding an integer to the list and see what happens and fix any potential issues.*/
@@ -28,8 +30,11 @@ class MainActivity : AppCompatActivity() {
     To finish this task, you will need to import rxjava, rxAndroid dependencies using gradle.
 */
 
-        var observableInt =Observable.just("1","2","3")
-        var observableActors = Observable.just(BradPit("id","never talk about fight club"),TomCruise("notid","impossible"))
+        var observableInt = Observable.just("1", "2", "3")
+        var observableActors = Observable.just(
+            BradPit("id", "never talk about fight club"),
+            TomCruise("notid", "impossible")
+        )
 
         /*  Task 8
 
@@ -41,11 +46,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-/*    Task 9
-    Create a dummy network API like the one in the guided project to return four movies with custom attributes such as year of release, title, language, imdb rating (feel free to be creative here!, you will need to create a data class for Movie to start this task)
-    Make a call to this NetworkAPI to fetch the movie data and then observe the response and print it to the console.
-
-*/
         val exampleMutableList = mutableListOf<String>("1")
         //I can't add an int of course!
         //    stringClass(1) cant do that either
@@ -54,11 +54,12 @@ class MainActivity : AppCompatActivity() {
         val step4 = containerClass(this).stringCheck("bah hum bug")
         exampleMutableList.add(step4)
         Toast.makeText(this, "$step4 -- step4 worked", Toast.LENGTH_LONG).show()
+
     }
 
     /*       Task 2
-    Create a class that accepts a particular type.
-    Define the class and the type and see what happens when you pass an object that is of type other than the given one.*/
+Create a class that accepts a particular type.
+Define the class and the type and see what happens when you pass an object that is of type other than the given one.*/
     class stringClass(str: String) {
         var getter: String = str
 
@@ -69,10 +70,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     /*        Task 3
-        Create a Function that can specify the type of arguments it can accept
-        Check to see what happens if you pass on object that is not of this type.*/
+    Create a Function that can specify the type of arguments it can accept
+    Check to see what happens if you pass on object that is not of this type.*/
 
 
     class containerClass(context: Context) {
@@ -88,29 +88,30 @@ class MainActivity : AppCompatActivity() {
             ).show()
 
             /*        Task 4
-             Steps
-             Create a function that returns a certain type
-             Expose this function to another class which can then directly call this function and retrive the object returned by the function*/
+         Steps
+         Create a function that returns a certain type
+         Expose this function to another class which can then directly call this function and retrive the object returned by the function*/
 
 
         }
     }
 
     /*
-    Steps
+Steps
 
-    Enforce Generic Constraints by creating a class and denoting the type of objects it can hold.
-    To finish this task 5, create a class Phone that only accepts objects of type CellularService.
-    */
+Enforce Generic Constraints by creating a class and denoting the type of objects it can hold.
+To finish this task 5, create a class Phone that only accepts objects of type CellularService.
+*/
 /*    Steps
 
 Enforce restricted types for a class to related types of objects.
 To finish task 6, after task 5, refactor class Phone so that it can accept two types of CellularService like TMobile and Verizon.*/
     open class CellularService(val id: String)
-   open class Person()
-   open class Actor (open val id: String):Person()
-    data class TomCruise (override var id:String, val misson:String):Actor(id)
-    data class BradPit  (override var id:String, val fightClub:String):Actor(id)
+
+    open class Person()
+    open class Actor(open val id: String) : Person()
+    data class TomCruise(override var id: String, val misson: String) : Actor(id)
+    data class BradPit(override var id: String, val fightClub: String) : Actor(id)
 
     class Tmobile(id: String) : CellularService(id)
     class Verizon(id: String) : CellularService(id)
@@ -120,7 +121,6 @@ To finish task 6, after task 5, refactor class Phone so that it can accept two t
 
         }
     }
-
 
 
 }
